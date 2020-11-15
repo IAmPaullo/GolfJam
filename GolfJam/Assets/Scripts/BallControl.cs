3673f851debc11425375038b690471a69b524932
Paullo <paullotsw@gmail.com>
2020-11-15 10:27:16 -0300

    terminei o primeiro level mais ou menos

--- GolfJam/Assets/Scripts/BallControl.cs
+++ GolfJam/Assets/Scripts/BallControl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using TMPro;
 using UnityEngine;
 
 public class BallControl : MonoBehaviour
@@ -9,13 +10,20 @@ public class BallControl : MonoBehaviour
     [SerializeField] float multiplier;
     [SerializeField] Slider slider;
     [SerializeField] float maxBallForce;
+    [SerializeField] float terminalVelocity;
+    [SerializeField] TextMeshProUGUI score, rank;
+    [SerializeField] GameObject panel;
+
+    BallScore bscore;
     Vector3 mousePosition;
     bool ballTap;
     Rigidbody2D rbBall;
+    int ballHit;
 
     private void Start()
     {
         rbBall = GetComponent<Rigidbody2D>();
+        
         
     }
 
@@ -28,15 +36,17 @@ public class BallControl : MonoBehaviour
         if (ballForce > maxBallForce)
             ballForce = maxBallForce;
 
-
-
         if (Input.GetMouseButtonUp(0))
-        {
             ballTap = true;
-        }
+
+        
+        
 
         SlideManager();
-        
+
+        BallHitManager();
+
+
     }
     private void FixedUpdate()
     {
@@ -62,6 +72,64 @@ public class BallControl : MonoBehaviour
 
         slider.maxValue = maxBallForce;
         slider.value = ballForce;
+
+    }
+    private enum BallScore { Bad, OK, Good, Great, Excelent, Impossible };
+    void BallHitManager()
+    {
+
+        if (ballHit >= 7)
+            bscore = BallScore.Bad;
+
+        if (ballHit >= 5)
+            bscore = BallScore.Good;
+
+        if (ballHit >= 3)
+            bscore = BallScore.Great;
+
+        if (ballHit >= 2)
+            bscore = BallScore.Excelent;
+
+        if (ballHit >= 1)
+            bscore = BallScore.Impossible;
+
+        switch (bscore)
+        {
+            case BallScore.Bad:
+                rank.text = "Bad";
+                break;
+
+            case BallScore.Good:
+                rank.text = "Good";
+                break;
+
+            case BallScore.Great:
+                rank.text = "Great";
+                break;
+
+            case BallScore.Excelent:
+                rank.text = "Excelent";
+                break;
+
+            case BallScore.Impossible:
+                rank.text = "STOP CHEATING";
+                break;
+
+        }
+
+        score.text = ballHit.ToString();
+    }
+
+    IEnumerator EndLevel(int time)
+    { 
+        yield return new WaitForSeconds(3f);
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        panel.SetActive(true);
 
     }
 
