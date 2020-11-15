bbd3c14a0995a013152212e5d5033921de9957cb
Paullo <paullotsw@gmail.com>
2020-11-15 04:05:57 -0300

    Bola Pronta

--- GolfJam/Assets/Scripts/BallControl.cs
+++ GolfJam/Assets/Scripts/BallControl.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class BallControl : MonoBehaviour
 {
     [SerializeField] float ballForce;
     [SerializeField] float multiplier;
+    [SerializeField] Slider slider;
+    [SerializeField] float maxBallForce;
     Vector3 mousePosition;
     bool ballTap;
     Rigidbody2D rbBall;
@@ -13,7 +16,7 @@ public class BallControl : MonoBehaviour
     private void Start()
     {
         rbBall = GetComponent<Rigidbody2D>();
-
+        
     }
 
 
@@ -21,13 +24,19 @@ public class BallControl : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
             ballForce += Time.deltaTime * multiplier;
-            
-        
+
+        if (ballForce > maxBallForce)
+            ballForce = maxBallForce;
+
+
 
         if (Input.GetMouseButtonUp(0))
         {
             ballTap = true;
         }
+
+        SlideManager();
+        
     }
     private void FixedUpdate()
     {
@@ -43,5 +52,17 @@ public class BallControl : MonoBehaviour
 
     }
 
+
+
+    void SlideManager()
+    {
+        //float sliderClamp = Mathf.Clamp(ballForce, 0, maxBallForce);
+        //int sliderClamp = Mathf.Clamp01(ballForce);
+        //slider.value = sliderClamp;
+
+        slider.maxValue = maxBallForce;
+        slider.value = ballForce;
+
+    }
 
 }
